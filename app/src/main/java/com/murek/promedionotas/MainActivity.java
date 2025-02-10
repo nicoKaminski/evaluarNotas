a9a5d8931583d966351df6e0a2de80127e9a319e
Nicolas Kaminski <murek28@gmail.com>
2025-02-10 16:27:09 -0300

    update a binding

--- app/src/main/java/com/murek/promedionotas/MainActivity.java
+++ app/src/main/java/com/murek/promedionotas/MainActivity.java
@@ -7,30 +7,26 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.murek.promedionotas.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
-    private EditText et2;
-    private EditText et3;
-    private TextView tvResult;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        et1 = findViewById(R.id.etNumero1);
-        et2 = findViewById(R.id.etNumero2);
-        et3 = findViewById(R.id.etNumero3);
-        tvResult = findViewById(R.id.tvResultado);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 
     public void evaluar(View view) {
-        String num1 = et1.getText().toString();
-        String num2 = et2.getText().toString();
-        String num3 = et3.getText().toString();
+        String num1 = binding.etNumero1.getText().toString();
+        String num2 = binding.etNumero2.getText().toString();
+        String num3 = binding.etNumero3.getText().toString();
 
         int n1 = Integer.parseInt(num1);
         int n2 = Integer.parseInt(num2);
@@ -39,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         int res = (n1 + n2 + n3)/3;
 
         if (res >= 7){
-            tvResult.setText("Aprobado");
+            binding.tvResultado.setText("Aprobado");
         } else {
-            tvResult.setText("Reprobado");
+            binding.tvResultado.setText("Reprobado");
         }
     }
 }
